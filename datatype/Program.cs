c983d5288db1d203d1210c990237f6d843d28845
yongjohnlee80 <yongjohnlee80@outlook.com>
2021-11-04 23:29:40 -0700

    new folder

---
+++ datatype/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace datatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
