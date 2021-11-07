d7266ea317edfeee97b1ed55dee91b31e9f0b52b
yongjohnlee80 <yongjohnlee80@outlook.com>
2021-11-06 19:57:29 -0700

    done

--- switchexp/Program.cs
+++ switchexp/Program.cs
@@ -4,9 +4,20 @@ namespace switchexp
 {
     class Program
     {
+        static decimal CalculateTax(decimal amount, string regionalCode) {
+            var rate = regionalCode switch {
+                "CH" => 0.08M,
+                "DK" => 0.25M,
+                "GB" => 0.2M,
+                "HU" => 0.27M,
+                "CA" => 0.0825M,
+                _ => 0.07M
+            };
+            return amount * rate;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(CalculateTax(135.0M, "CA"));
         }
     }
 }
